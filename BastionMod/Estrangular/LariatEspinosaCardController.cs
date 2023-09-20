5d6171baa947a344c57ab0d6f225b3b034ddc030
kree1 <myopicmastermind@gmail.com>
2023-09-20 12:27:28 -0400

    Fixed "Lariat" Espinosa's damage criteria

--- BastionMod/Estrangular/LariatEspinosaCardController.cs
+++ BastionMod/Estrangular/LariatEspinosaCardController.cs
@@ -24,7 +24,7 @@ namespace Bastion.Estrangular
             // "{human} At the start of the villain turn, this card deals the hero target with the second lowest HP 2 melee damage."
             AddStartOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, (PhaseChangeAction pca) => DealDamageToLowestHP(base.Card, 2, (Card c) => IsHeroTarget(c), (Card c) => 2, DamageType.Melee), TriggerType.DealDamage, (PhaseChangeAction pca) => ActivateHuman);
             // "{snake} Reduce damage dealt by [i]Estrangular[/i] by 1."
-            AddReduceDamageTrigger((DealDamageAction dda) => ActivateSnake && dda.DamageSource.IsCard && dda.DamageSource.Card == base.CharacterCard && base.CharacterCard.IsFlipped, (DealDamageAction dda) => 1);
+            AddReduceDamageTrigger((DealDamageAction dda) => ActivateSnake && dda.DamageSource != null && dda.DamageSource.IsCard && dda.DamageSource.Card == base.CharacterCard && base.CharacterCard.IsFlipped, (DealDamageAction dda) => 1);
         }
     }
 }
