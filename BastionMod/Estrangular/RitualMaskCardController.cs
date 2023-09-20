df7d6c8c297a2d8fac5c4b8c4b808b8380ce44e0
kree1 <myopicmastermind@gmail.com>
2023-09-20 12:27:10 -0400

    Added Ritual Mask and Sacrificial Dagger CardControllers

---
+++ BastionMod/Estrangular/RitualMaskCardController.cs
@@ -0,0 +1,58 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bastion.Estrangular
+{
+    public class RitualMaskCardController : CardController
+    {
+        public RitualMaskCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "This card is immune to damage dealt by villain targets."
+            AddImmuneToDamageTrigger((DealDamageAction dda) => dda.Target == base.Card && dda.DamageSource != null && dda.DamageSource.IsCard && IsVillainTarget(dda.DamageSource.Card));
+            // "Damage dealt by [i]Estrangular[/i] is irreducible."
+            AddMakeDamageIrreducibleTrigger((DealDamageAction dda) => dda.DamageSource != null && dda.DamageSource.IsCard && dda.DamageSource.Card == base.CharacterCard && base.CharacterCard.IsFlipped);
+            // "At the end of the villain turn, each player may discard a card. Each hero whose player did not discard a card this way deals themself 1 psychic damage."
+            AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, DiscardOrDealSelfDamageResponse, new TriggerType[] { TriggerType.DiscardCard, TriggerType.DealDamage });
+        }
+
+        private IEnumerator DiscardOrDealSelfDamageResponse(PhaseChangeAction pca)
+        {
+            // "... each player may discard a card."
+            List<DiscardCardAction> discardResults = new List<DiscardCardAction>();
+            IEnumerator discardCoroutine = base.GameController.EachPlayerDiscardsCards(0, 1, discardResults, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(discardCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(discardCoroutine);
+            }
+            // "Each hero whose player did not discard a card this way deals themself 1 psychic damage."
+            List<HeroTurnTakerController> didNotDiscardPlayers = FindActiveHeroTurnTakerControllers().Except(from d in discardResults where d.WasCardDiscarded select d.HeroTurnTakerController).ToList();
+            List<Card> didNotDiscardHeroes = FindCardsWhere((Card c) => IsHeroCharacterCard(c) && c.IsTarget && c.Owner.IsHero && didNotDiscardPlayers.Contains(base.GameController.FindTurnTakerController(c.Owner).ToHero())).ToList();
+            IEnumerator psychicCoroutine = base.GameController.DealDamageToSelf(DecisionMaker, (Card c) => didNotDiscardHeroes.Contains(c), (Card c) => 1, DamageType.Psychic, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(psychicCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(psychicCoroutine);
+            }
+        }
+    }
+}
---
+++ BastionMod/Estrangular/SacrificialDaggerCardController.cs
@@ -0,0 +1,63 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bastion.Estrangular
+{
+    public class SacrificialDaggerCardController : CardController
+    {
+        public SacrificialDaggerCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            // Show villain target with highest HP
+            SpecialStringMaker.ShowVillainTargetWithHighestHP();
+            // Show hero target with second highest HP
+            SpecialStringMaker.ShowHeroTargetWithHighestHP(2);
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "This card is immune to damage dealt by villain targets."
+            AddImmuneToDamageTrigger((DealDamageAction dda) => dda.Target == base.Card && dda.DamageSource != null && dda.DamageSource.IsCard && IsVillainTarget(dda.DamageSource.Card));
+            // "Increase damage dealt by [i]Estrangular[/i] by 1."
+            AddIncreaseDamageTrigger((DealDamageAction dda) => dda.DamageSource != null && dda.DamageSource.IsCard && dda.DamageSource.Card == base.CharacterCard && base.CharacterCard.IsFlipped, (DealDamageAction dda) => 1);
+            // "At the end of the villain turn, the villain target with the highest HP deals the hero target with the second highest HP 2 melee damage."
+            AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, MeleeDamageResponse, TriggerType.DealDamage);
+        }
+
+        private IEnumerator MeleeDamageResponse(PhaseChangeAction pca)
+        {
+            // "... the villain target with the highest HP..."
+            List<Card> storedResults = new List<Card>();
+            IEnumerator findCoroutine = base.GameController.FindTargetWithHighestHitPoints(1, (Card c) => IsVillainTarget(c), storedResults, cardSource: GetCardSource());
+            if (base.UseUnityCoroutines)
+            {
+                yield return base.GameController.StartCoroutine(findCoroutine);
+            }
+            else
+            {
+                base.GameController.ExhaustCoroutine(findCoroutine);
+            }
+            Card source = storedResults.FirstOrDefault();
+            if (source != null)
+            {
+                // "... deals the hero target with the second highest HP 2 melee damage."
+                IEnumerator damageCoroutine = DealDamageToHighestHP(source, 2, (Card c) => IsHeroTarget(c), (Card c) => 2, DamageType.Melee);
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(damageCoroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(damageCoroutine);
+                }
+            }
+        }
+    }
+}
