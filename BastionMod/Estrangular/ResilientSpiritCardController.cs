c97b1de000fc80898a5762cf6a2efcb30a48a4e8
kree1 <myopicmastermind@gmail.com>
2023-09-20 14:21:49 -0400

    Added Resilient Spirit and Viper's Venom CardControllers

---
+++ BastionMod/Estrangular/ResilientSpiritCardController.cs
@@ -0,0 +1,80 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bastion.Estrangular
+{
+    public class ResilientSpiritCardController : HumanSnakeUtilityCardController
+    {
+        public ResilientSpiritCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            // Show whether redirection has been used this turn
+            SpecialStringMaker.ShowHasBeenUsedThisTurn(FirstRicoDamageThisTurn, base.Card.Title + " can no longer redirect damage from [i]Rico Homem[/i] this turn.", base.Card.Title + " has not redirected damage from [i]Rico Homem[/i] this turn.");
+            // Show non-villain target with lowest HP
+            SpecialStringMaker.ShowNonVillainTargetWithLowestHP();
+            // Show whether retaliation has been used this turn
+            SpecialStringMaker.ShowHasBeenUsedThisTurn(FirstMonsterDamageThisTurn, base.Card.Title + " has already responded to damage dealt to [i]Estrangular[/i] this turn.", base.Card.Title + " has not responded to damage dealt to [i]Estrangular[/i] this turn.");
+        }
+
+        protected readonly string FirstRicoDamageThisTurn = "FirstRicoDamageThisTurn";
+        protected readonly string FirstMonsterDamageThisTurn = "FirstMonsterDamageThisTurn";
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "{human} The first time [i]Rico Homem[/i] would be dealt damage each turn, redirect that damage to the non-villain target with the lowest HP."
+            AddTrigger((DealDamageAction dda) => !HasBeenSetToTrueThisTurn(FirstRicoDamageThisTurn) && dda.Target == base.CharacterCard && !base.CharacterCard.IsFlipped, IfHumanRedirectResponse, TriggerType.RedirectDamage, TriggerTiming.Before);
+            AddAfterLeavesPlayAction((GameAction ga) => ResetFlagAfterLeavesPlay(FirstRicoDamageThisTurn), TriggerType.Hidden);
+            // "{snake} The first time [i]Estrangular[/i] would be dealt damage each turn, he deals the source of that damage {H - 2} melee damage."
+            AddTrigger((DealDamageAction dda) => !HasBeenSetToTrueThisTurn(FirstMonsterDamageThisTurn) && dda.Target == base.CharacterCard && base.CharacterCard.IsFlipped, IfSnakeRetaliateResponse, TriggerType.DealDamage, TriggerTiming.Before);
+            AddAfterLeavesPlayAction((GameAction ga) => ResetFlagAfterLeavesPlay(FirstMonsterDamageThisTurn), TriggerType.Hidden);
+        }
+
+        private IEnumerator IfHumanRedirectResponse(DealDamageAction dda)
+        {
+            SetCardPropertyToTrueIfRealAction(FirstRicoDamageThisTurn, gameAction: dda);
+            if (ActivateHuman)
+            {
+                // "... redirect that damage to the non-villain target with the lowest HP."
+                IEnumerator redirectCoroutine = RedirectDamage(dda, TargetType.LowestHP, (Card c) => !IsVillainTarget(c));
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(redirectCoroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(redirectCoroutine);
+                }
+            }
+            yield break;
+        }
+
+        private IEnumerator IfSnakeRetaliateResponse(DealDamageAction dda)
+        {
+            SetCardPropertyToTrueIfRealAction(FirstMonsterDamageThisTurn, gameAction: dda);
+            if (ActivateSnake)
+            {
+                if (dda.DamageSource != null && dda.DamageSource.IsTarget)
+                {
+                    // "... he deals the source of that damage {H - 2} melee damage."
+                    IEnumerator meleeCoroutine = DealDamage(base.CharacterCard, dda.DamageSource.Card, H - 2, DamageType.Melee, isCounterDamage: true, cardSource: GetCardSource());
+                    if (base.UseUnityCoroutines)
+                    {
+                        yield return base.GameController.StartCoroutine(meleeCoroutine);
+                    }
+                    else
+                    {
+                        base.GameController.ExhaustCoroutine(meleeCoroutine);
+                    }
+                }
+            }
+            yield break;
+        }
+    }
+}
---
+++ BastionMod/Estrangular/VipersVenomCardController.cs
@@ -0,0 +1,63 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bastion.Estrangular
+{
+    public class VipersVenomCardController : HumanSnakeUtilityCardController
+    {
+        public VipersVenomCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            // Show whether damage has been dealt by a villain target yet this turn
+            SpecialStringMaker.ShowHasBeenUsedThisTurn(FirstVillainDamageThisTurn, "A villain target has already dealt damage this turn since " + base.Card.Title + " entered play.", "No villain targets have dealt damage this turn since " + base.Card.Title + "entered play.").Condition = () => base.Card.IsInPlayAndHasGameText;
+        }
+
+        protected readonly string FirstVillainDamageThisTurn = "FirstVillainDamageThisTurn";
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "{human} The first time any villain target deals damage each turn, increase that damage by 2 and change its type to toxic."
+            AddTrigger((DealDamageAction dda) => !HasBeenSetToTrueThisTurn(FirstVillainDamageThisTurn) && dda.DamageSource != null && dda.DamageSource.IsVillainTarget, IfHumanIncreaseModifyResponse, new TriggerType[] { TriggerType.IncreaseDamage, TriggerType.ChangeDamageType }, TriggerTiming.Before);
+            AddAfterLeavesPlayAction((GameAction ga) => ResetFlagAfterLeavesPlay(FirstVillainDamageThisTurn), TriggerType.Hidden);
+            // "{snake} Change all damage dealt by [i]Estrangular[/i] to Toxic. Increase damage dealt by [i]Estrangular[/i] by 1."
+            AddChangeDamageTypeTrigger((DealDamageAction dda) => ActivateSnake && dda.DamageSource != null && dda.DamageSource.IsCard && dda.DamageSource.Card == base.CharacterCard && base.CharacterCard.IsFlipped, DamageType.Toxic);
+            AddIncreaseDamageTrigger((DealDamageAction dda) => ActivateSnake && dda.DamageSource != null && dda.DamageSource.IsCard && dda.DamageSource.Card == base.CharacterCard && base.CharacterCard.IsFlipped, (DealDamageAction dda) => 1);
+        }
+
+        private IEnumerator IfHumanIncreaseModifyResponse(DealDamageAction dda)
+        {
+            SetCardPropertyToTrueIfRealAction(FirstVillainDamageThisTurn, gameAction: dda);
+            if (ActivateHuman)
+            {
+                // "{human} ... increase that damage by 2..."
+                IEnumerator increaseCoroutine = base.GameController.IncreaseDamage(dda, (DealDamageAction d) => 2, cardSource: GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(increaseCoroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(increaseCoroutine);
+                }
+                // "... and change its type to toxic."
+                IEnumerator changeCoroutine = base.GameController.ChangeDamageType(dda, DamageType.Toxic, cardSource: GetCardSource());
+                if (base.UseUnityCoroutines)
+                {
+                    yield return base.GameController.StartCoroutine(changeCoroutine);
+                }
+                else
+                {
+                    base.GameController.ExhaustCoroutine(changeCoroutine);
+                }
+            }
+            yield break;
+        }
+    }
+}
