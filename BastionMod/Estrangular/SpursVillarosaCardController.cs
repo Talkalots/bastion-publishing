25f48e6399be5d3e0c8a0bed58161cb51362b8fa
kree1 <myopicmastermind@gmail.com>
2023-09-20 11:48:32 -0400

    Added "Spurs" Villarosa and "Toro" Rocco CardControllers

---
+++ BastionMod/Estrangular/SpursVillarosaCardController.cs
@@ -0,0 +1,30 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bastion.Estrangular
+{
+    public class SpursVillarosaCardController : HumanSnakeUtilityCardController
+    {
+        public SpursVillarosaCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            // Show hero target with highest HP
+            SpecialStringMaker.ShowHeroTargetWithHighestHP();
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "{human} At the end of the villain turn, this card deals the hero target with the highest HP 2 melee damage."
+            AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, (PhaseChangeAction pca) => DealDamageToHighestHP(base.Card, 1, (Card c) => IsHeroTarget(c), (Card c) => 2, DamageType.Melee), TriggerType.DealDamage, (PhaseChangeAction pca) => ActivateHuman);
+            // "{snake} At the start of each hero turn, this card deals [i]Estrangular[/i] 1 melee damage."
+            AddStartOfTurnTrigger((TurnTaker tt) => tt.IsHero, (PhaseChangeAction pca) => DealDamage(base.Card, base.CharacterCard, 1, DamageType.Melee, cardSource: GetCardSource()), TriggerType.DealDamage, (PhaseChangeAction pca) => ActivateSnake);
+        }
+    }
+}
---
+++ BastionMod/Estrangular/ToroRoccoCardController.cs
@@ -0,0 +1,32 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bastion.Estrangular
+{
+    public class ToroRoccoCardController : HumanSnakeUtilityCardController
+    {
+        public ToroRoccoCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            // Show non-villain target with highest HP
+            SpecialStringMaker.ShowNonVillainTargetWithHighestHP();
+            // Show villain target with highest HP
+            SpecialStringMaker.ShowVillainTargetWithHighestHP();
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "{human} At the end of the villain turn, this card deals the non-villain target with the highest HP {H - 1} melee damage."
+            AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, (PhaseChangeAction pca) => DealDamageToHighestHP(base.Card, 1, (Card c) => !IsVillainTarget(c), (Card c) => H - 1, DamageType.Melee), TriggerType.DealDamage, (PhaseChangeAction pca) => ActivateHuman);
+            // "{snake} At the start of the villain turn, this card deals the villain target with the highest HP {H - 1} melee damage."
+            AddStartOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, (PhaseChangeAction pca) => DealDamageToHighestHP(base.Card, 1, (Card c) => IsVillainTarget(c), (Card c) => H - 1, DamageType.Melee), TriggerType.DealDamage, (PhaseChangeAction pca) => ActivateSnake);
+        }
+    }
+}
