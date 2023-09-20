9c8f7a4dd140af39f96e03e2af373e3298c6019d
kree1 <myopicmastermind@gmail.com>
2023-09-20 11:38:41 -0400

    Added "Lariat" Espinosa and Poachers CardControllers

---
+++ BastionMod/Estrangular/LariatEspinosaCardController.cs
@@ -0,0 +1,30 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bastion.Estrangular
+{
+    public class LariatEspinosaCardController : HumanSnakeUtilityCardController
+    {
+        public LariatEspinosaCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            // Show hero target with second lowest HP
+            SpecialStringMaker.ShowHeroTargetWithLowestHP(2);
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "{human} At the start of the villain turn, this card deals the hero target with the second lowest HP 2 melee damage."
+            AddStartOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, (PhaseChangeAction pca) => DealDamageToLowestHP(base.Card, 2, (Card c) => IsHeroTarget(c), (Card c) => 2, DamageType.Melee), TriggerType.DealDamage, (PhaseChangeAction pca) => ActivateHuman);
+            // "{snake} Reduce damage dealt by [i]Estrangular[/i] by 1."
+            AddReduceDamageTrigger((DealDamageAction dda) => ActivateSnake && dda.DamageSource.IsCard && dda.DamageSource.Card == base.CharacterCard && base.CharacterCard.IsFlipped, (DealDamageAction dda) => 1);
+        }
+    }
+}
---
+++ BastionMod/Estrangular/PoachersCardController.cs
@@ -0,0 +1,30 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bastion.Estrangular
+{
+    public class PoachersCardController : HumanSnakeUtilityCardController
+    {
+        public PoachersCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+            // Show non-villain target with lowest HP
+            SpecialStringMaker.ShowNonVillainTargetWithLowestHP();
+        }
+
+        public override void AddTriggers()
+        {
+            base.AddTriggers();
+            // "{human} At the end of the villain turn, this card deals the non-villain target with the lowest HP 2 projectile damage."
+            AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, (PhaseChangeAction pca) => DealDamageToLowestHP(base.Card, 1, (Card c) => !IsVillainTarget(c), (Card c) => 2, DamageType.Projectile), TriggerType.DealDamage, (PhaseChangeAction pca) => ActivateHuman);
+            // "{snake} At the end of the villain turn, this card deals [i]Estrangular[/i] 2 projectile damage."
+            AddEndOfTurnTrigger((TurnTaker tt) => tt == base.TurnTaker, (PhaseChangeAction pca) => DealDamage(base.Card, base.CharacterCard, 2, DamageType.Projectile, cardSource: GetCardSource()), TriggerType.DealDamage, (PhaseChangeAction pca) => ActivateSnake && base.CharacterCard.IsFlipped);
+        }
+    }
+}
